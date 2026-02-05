db7b361b2912d1908572b41e0dcd03ce5939f3da
syedwajidbembexlab-ui <syedwajidbembexlab@gmail.com>
2026-02-05 11:27:30 +0000

    Conversion of datatypes

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,9 +1,29 @@
 let score = false
 
-console.log(typeof score)   // number
-console.log(typeof (score)) // 
+//console.log(typeof score)   // number
+//console.log(typeof (score)) // 
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber) // number
-console.log(valueInNumber) // NaN (Not a Number) because "33abc" is not a valid number string
+//console.log(typeof valueInNumber) // number
+//console.log(valueInNumber) // NaN (Not a Number) because "33abc" is not a valid number string
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn) // true because 1 is a truthy value
+
+//1 => true
+//0 => false
+//"" => false
+//"abc" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber) // string
+console.log(stringNumber) // "33" number converted to string
 
